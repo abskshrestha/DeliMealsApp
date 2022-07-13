f2a93b62fab00c42a935d665c93d0f9a73d3344e
Abhishek Shrestha <absk.shrestha@gmail.com>
2022-07-13 18:45:53 +0545

    passing data and filter switches

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,10 +1,33 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/widgets/main_drawer.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   // const FiltersScreen({ Key? key }) : super(key: key);
 
   static const routeName = '/filters';
+
+  @override
+  State<FiltersScreen> createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  var _glutenFree = false;
+  var _vegan = false;
+  var _vegetarian = false;
+  var _lactoseFree = false;
+
+  Widget _buildSwitchListTile(String title, String description,
+      bool currentValue, Function updateValue) {
+    return SwitchListTile(
+      title: Text(title),
+      value: currentValue,
+      subtitle: Text(description),
+      onChanged: updateValue,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,7 +35,64 @@ class FiltersScreen extends StatelessWidget {
         title: Text('Your Filters'),
       ),
       drawer: MainDrawer(),
-      body: Center(child: Text('filters')),
+      body: Column(
+        children: [
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Adjust your meal selection',
+                  style: Theme.of(context).textTheme.headline6),
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                _buildSwitchListTile(
+                  "Gluten-free",
+                  "Only includes meals that are glueten free",
+                  _glutenFree,
+                  (newValue) {
+                    setState(() {
+                      _glutenFree = newValue;
+                    });
+                  },
+                ),
+                 _buildSwitchListTile(
+                  "Vegan",
+                  "Only includes meals that are Vegan",
+                  _vegan,
+                  (newValue) {
+                    setState(() {
+                      _vegan = newValue;
+                    });
+                  },
+                ),
+                 _buildSwitchListTile(
+                  "Vegetarian",
+                  "Only includes meals that are vegetarian",
+                  _vegetarian,
+                  (newValue) {
+                    setState(() {
+                      _vegetarian = newValue;
+                    });
+                  },
+                ),
+                 _buildSwitchListTile(
+                  "Lactose-free",
+                  "Only includes meals that are Lactose free",
+                  _lactoseFree,
+                  (newValue) {
+                    setState(() {
+                      _lactoseFree = newValue;
+                    });
+                  },
+                ),
+
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -79,7 +79,7 @@ class MealDetailScreen extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.delete),
         onPressed: () {
-          Navigator.of(context).pop();
+          Navigator.of(context).pop(mealId);
         },
       ),
     );
